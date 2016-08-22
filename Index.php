f7a7d305fdb14e68fa11c6ee99042284db27d0fe
andinamatias <andinamatias@gmail.com>
2016-08-22 20:38:08 -0300

    asd

--- Index.php
+++ Index.php
@@ -6,7 +6,37 @@
 hola mundo <!--lo que escribvo aca es codigo php-->
 <?php
 
-echo "hola php";
+$nombre = "Matias";
+$veces = 10;
+$cont = 0;
+
+echo "<h2> hola php soy '$nombre'</h2>";
+
+echo(rand() . "<br>"); // numero random (aleatorio).
+echo(rand(10,100)); // numero random entre 10 y 100.
+
+while($cont < $veces)
+{
+
+echo(rand(0,9) . "<br>");
+echo "'$cont'" . "<br>";
+$cont = $cont +1;
+
+switch ($cont) {
+	case 1:
+		echo "es uno"
+		break;
+	case 2:
+		echo "es dos"
+		break;
+	default:
+		# code...
+		break;
+}
+
+
+
+}
 
 ?>
 </body>
