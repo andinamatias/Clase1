782fe08fd0a62df899210f20fc18ddf2ccc8f323
andinamatias <andinamatias@gmail.com>
2016-08-22 21:18:43 -0300

    Se guarda while, se debe sacar el porcentaje de veces que sale un numero

--- Index.php
+++ Index.php
@@ -9,34 +9,43 @@ hola mundo <!--lo que escribvo aca es codigo php-->
 $nombre = "Matias";
 $veces = 10;
 $cont = 0;
+$porc = 0;
+$numeroveces = 0;
+$random;
 
-echo "<h2> hola php soy '$nombre'</h2>";
+//echo "<h2> hola php soy '$nombre'</h2>";
 
-echo(rand() . "<br>"); // numero random (aleatorio).
-echo(rand(10,100)); // numero random entre 10 y 100.
+//echo(rand() . "<br>"); // numero random (aleatorio).
+//echo(rand(10,100)); // numero random entre 10 y 100.
 
-while($cont < $veces)
-{
+	echo "while";
+	while($cont <= $veces)
+	{
+		
+	$random = rand(0,9);
+	echo(rand(0,9) . "<br>");
+	echo "'$cont'" . "<br>";
+	$cont = $cont +1;
 
-echo(rand(0,9) . "<br>");
-echo "'$cont'" . "<br>";
-$cont = $cont +1;
+		switch ($random) {
+			case 1:
+				echo "es uno" . "<br>";
+				$numeroveces = $numeroveces + 1;
+				break;
+			case 2:
+				echo "es dos" . "<br>";
+				break;
+			default:
+			//	# code...
+				break;
 
-switch ($cont) {
-	case 1:
-		echo "es uno"
-		break;
-	case 2:
-		echo "es dos"
-		break;
-	default:
-		# code...
-		break;
-}
+		}
 
+	}
 
+$porc = $numeroveces * $veces / 100;
 
-}
+echo "$porc";
 
 ?>
 </body>
